4f6e0b1287602deb3cacca481394f41b69b1fecf
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-12-25 00:49:09 -0500

    Fixed rescale script

--- rescale.py
+++ rescale.py
@@ -1,10 +1,20 @@
+import argparse, os
 import numpy as np
-import afqstensorutils as atu
+from . import Scaler
 
-dat = np.loadtxt("surf.csv",skiprows=1,delimiter=",")
-with file("surf.csv") as f:
+parser = argparse.ArgumentParser(description='Scale a csv.')
+parser.add_argument('file', metavar='file', type=str,
+        help='file to scale')
+args = parser.parse_args()
+fname, ext = os.path.splitext(args.file)
+
+with file(fname+ext) as f:
     header = f.readline()[:-1]
+dat = np.loadtxt(fname+ext,skiprows=1,delimiter=",")
     
-S = atu.Scaler(dat)
+S = Scaler(dat)
 
-np.savetxt("surf_scaled.csv",S.scaled_data,delimiter=", ",header=header,comments="")
+np.savetxt(fname+"_scaled"+ext,S.scaled_data,
+           delimiter=",",header=header,comments="")
+np.savetxt(fname+"_ranges"+ext,np.array(S.scale).T,
+           delimiter=",",header=header,comments="")
