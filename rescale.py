309e2cfa3740735865fb6d7aa9ac03badc56e447
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2019-01-08 01:24:59 -0800

    rescale makes the shards too

--- rescale.py
+++ rescale.py
@@ -1,10 +1,13 @@
 import argparse, os
 import numpy as np
 from . import Scaler
+from .dataprep import make_shards
 
-parser = argparse.ArgumentParser(description='Scale a csv.')
+parser = argparse.ArgumentParser(description='Scale a csv and shuffle into shards.')
 parser.add_argument('file', metavar='file', type=str,
         help='file to scale')
+parser.add_argument('datums_per_shard', metavar='datums_per_shard', type=int,
+        help='How many datums should go into a shard')
 args = parser.parse_args()
 fname, ext = os.path.splitext(args.file)
 
@@ -18,3 +21,6 @@ np.savetxt(fname+"_scaled"+ext,S.scaled_data,
            delimiter=",",header=header,comments="")
 np.savetxt(fname+"_ranges"+ext,np.array(S.scale).T,
            delimiter=",",header=header,comments="")
+
+if args.datums_per_shard>0:
+    make_shards(fname+"_scaled"+ext, fname+"_sharded", args.datums_per_shard)
